e70f83ac0924fc5d52e2cbec176d2ca55e3a6004
Wille Lee <1992season@gmail.com>
2023-10-16 13:56:23 +0900

    [ADD] firebase storage and db + challenge

--- src/components/Home/PostTweetForm.tsx
+++ src/components/Home/PostTweetForm.tsx
@@ -1,22 +1,58 @@
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
-import { FormProvider, useForm, useFormContext } from "react-hook-form";
+import {
+  FormProvider,
+  SubmitHandler,
+  useForm,
+  useFormContext,
+} from "react-hook-form";
+import { auth, db, storage } from "../../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 type TweetData = {
   tweet: string;
-  images: FileList | null;
+  image: File | null;
 };
 
 const PostTweetForm = () => {
   const [isLoading, setIsLoading] = useState(false);
   const methods = useForm<TweetData>({
-    defaultValues: { tweet: "", images: null },
+    defaultValues: { tweet: "", image: null },
   });
 
-  const onSubmit = async (formData: TweetData) => {
-    setIsLoading(true);
-    console.log(formData);
-    console.log(formData.images?.[0]);
-    setIsLoading(false);
+  const onSubmit: SubmitHandler<TweetData> = async (formData) => {
+    const user = auth.currentUser;
+    if (!user || isLoading || formData.tweet === "") return;
+    try {
+      setIsLoading(true);
+      if (formData.image && formData.image.size >= 1000000) {
+        alert("Image size should be less than 1MB");
+        return;
+      }
+      const doc = await addDoc(collection(db, "tweets"), {
+        tweet: formData.tweet,
+        createdAt: new Date(),
+        username: user.displayName || "Anonymous",
+        userId: user.uid,
+        image: null,
+      });
+      if (formData.image) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        const result = await uploadBytes(locationRef, formData.image);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc, {
+          image: url,
+        });
+      }
+      methods.reset();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -28,6 +64,7 @@ const PostTweetForm = () => {
           type="submit"
           disabled={isLoading}
           value={isLoading ? "Posting..." : "Tweet"}
+          style={{ cursor: "pointer" }}
         />
       </form>
     </FormProvider>
@@ -40,18 +77,30 @@ const PostTweetTextarea = React.memo(() => {
 });
 
 const PostTweetImage = React.memo(() => {
-  const { register, watch } = useFormContext<TweetData>();
-  const { images } = watch();
+  const { watch, setValue } = useFormContext<TweetData>();
+  const { image } = watch();
+  const handleChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { files } = e.target;
+    const file = files && files[0];
+    if (!file) return;
+    if (file.size >= 1000000) {
+      alert("Image size should be less than 1MB");
+      return;
+    }
+    setValue("image", file);
+  };
   return (
     <>
-      <label htmlFor="post-tweet-image">
-        {!!images && images.length > 0 ? "Change " : "Add "} Photo
+      <label htmlFor="post-tweet-image" style={{ cursor: "pointer" }}>
+        {image ? "Change " : "Add "} Photo
       </label>
       <input
         id="post-tweet-image"
         type="file"
         accept="image/*"
-        {...register("images")}
+        style={{ display: "none" }}
+        onChange={handleChangeImage}
       />
     </>
   );
